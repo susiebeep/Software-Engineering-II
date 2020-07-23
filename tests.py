edf216dd5b42fba0baf7dc7f0aef63209b07f190
Susan <hibbert.susan@gmail.com>
2020-07-23 11:24:18 -0700

    First test rejected empty string as password

--- tests.py
+++ tests.py
@@ -3,8 +3,10 @@ from check_pwd import check_pwd
 
 
 class TestCase(unittest.TestCase):
+    # check empty passwords
     def test_1(self):
-        return True
+        password = ''
+        self.assertFalse(check_pwd(password))
 
 
 if __name__ == '__main__':
