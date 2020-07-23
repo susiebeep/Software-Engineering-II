aec01a07ddaa92db122decd540e8946298edcc43
Susan <hibbert.susan@gmail.com>
2020-07-23 11:45:17 -0700

    Check_pwd rejects passwords longer than 20 chars

--- check_pwd.py
+++ check_pwd.py
@@ -3,7 +3,8 @@ def check_pwd(password):
     if len(password) == 0:
         return False
     # reject passwords less than 8 characters
-    if len(password) < 8:
+    # or greater than 20 characters
+    if len(password) < 8 or len(password) > 20:
         return False
     else:
         return True
