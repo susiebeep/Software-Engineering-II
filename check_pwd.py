b4e015a5e1b0595d8a078a02745ab3237bf4fd87
Susan <hibbert.susan@gmail.com>
2020-07-23 12:01:36 -0700

    Check_pwd rejects all uppercase passwords

--- check_pwd.py
+++ check_pwd.py
@@ -6,5 +6,9 @@ def check_pwd(password):
     # or greater than 20 characters
     if len(password) < 8 or len(password) > 20:
         return False
+    # check for at least one lowercase letter-
+    # if all chars in the password are uppercase
+    if password.isupper():
+        return False
     else:
         return True
