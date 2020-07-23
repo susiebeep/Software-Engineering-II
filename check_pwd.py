8f6b73fbc01744678681d455f61904eeed6c0a19
Susan <hibbert.susan@gmail.com>
2020-07-23 11:41:07 -0700

    Check_pwd rejects passwords shorter than 8 chars

--- check_pwd.py
+++ check_pwd.py
@@ -2,5 +2,8 @@ def check_pwd(password):
     # reject empty passwords
     if len(password) == 0:
         return False
+    # reject passwords less than 8 characters
+    if len(password) < 8:
+        return False
     else:
         return True
